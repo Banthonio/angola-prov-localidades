84afcd2ffa073e99c6531bffc9ed44d0d2e8ed9e
banthonio <braulio.andrew@gmail.com>
2021-08-25 11:53:35 +0100

    updated uri endpoints on routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -12,24 +12,24 @@ const cityController = new CityController();
 const communeController = new CommuneController();
 const districtController = new DistrictController();
 
-routes.get('/province', provinceController.index);
-routes.get('/province/code/:code', provinceController.showCode);
-routes.get('/province/name/:name', provinceController.showName);
-routes.get('/province/capital/:capital', provinceController.showCapital);
-routes.get('/province/lang/:lang', provinceController.showLang);
+routes.get('/provinces', provinceController.index);
+routes.get('/provinces/code/:code', provinceController.showCode);
+routes.get('/provinces/name/:name', provinceController.showName);
+routes.get('/provinces/capital/:capital', provinceController.showCapital);
+routes.get('/provinces/lang/:lang', provinceController.showLang);
 
-routes.get('/city/code/:code', cityController.showCode);
-routes.get('/city/name/:name', cityController.showName);
-
-// NOTE This must come before route that displays city (uses id)
-routes.get('/commune', communeController.index); //obtem o conjunto de comunas de Angola
-routes.get('/province/:province/commune', communeController.showProvince); // obtem o conjunto de comunas de Angola a partir da provincia
-routes.get('/city/:city/commune', communeController.showCity); // obtem o conjunto de comunas de Angola a partir da cidade
+routes.get('/cities/code/:code', cityController.showCode);
+routes.get('/cities/name/:name', cityController.showName);
 
 // NOTE This must come before route that displays city (uses id)
-routes.get('/district', districtController.index); //obtem o conjunto de distritos de Angola
-routes.get('/province/:province/district', districtController.showProvince); // obtem o conjunto de distritos de Angola a partir da provincia
-routes.get('/city/:city/district', districtController.showCity); // obtem o conjunto de distritos de Angola a partir da cidade
+routes.get('/communes', communeController.index); //obtem o conjunto de comunas de Angola
+routes.get('/provinces/:province/communes', communeController.showProvince); // obtem o conjunto de comunas de Angola a partir da provincia
+routes.get('/cities/:city/communes', communeController.showCity); // obtem o conjunto de comunas de Angola a partir da cidade
+
+// NOTE This must come before route that displays city (uses id)
+routes.get('/districts', districtController.index); //obtem o conjunto de distritos de Angola
+routes.get('/provinces/:province/districts', districtController.showProvince); // obtem o conjunto de distritos de Angola a partir da provincia
+routes.get('/cities/:city/districts', districtController.showCity); // obtem o conjunto de distritos de Angola a partir da cidade
 
 
 
